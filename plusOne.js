89eaaff325edc6e1b78439508424ea1da767fdb8
Chad Dornfeld <cmdornfeld@gmail.com>
2020-07-22 16:54:13 -0500

    added plusOne solution

---
+++ plusOne.js
@@ -0,0 +1,13 @@
+function plusOne(digits) {
+    for(let i=digits.length-1; i>=0; i--){
+        digits[i]++
+        if(digits[i] < 10){
+            return digits;
+        }
+        digits[i] = 0;
+        if(i === 0){
+            digits.unshift(1);
+            return digits;
+        }
+    }
+};
