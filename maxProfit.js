4abd84661e59baa09201001bc194a39a511cfa6d
Chad Dornfeld <cmdornfeld@gmail.com>
2020-08-07 11:28:00 -0500

    added maxProfit solution

---
+++ maxProfit.js
@@ -0,0 +1,13 @@
+function maxProfit(prices) {
+    let min = prices[0];
+    let max = 0;
+    for(let i=1; i<prices.length; i++){
+        if(prices[i] < min){
+            min = prices[i];
+        }
+        if(prices[i] - min > max){
+            max = prices[i] - min;
+        }
+    }
+    return max;
+};
